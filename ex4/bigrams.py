cb5dea960b8c4143fbeb63173ec6d7fdfeea3c5a
Michał Zakrzewski <miczak96@gmail.com>
2019-05-06 16:41:43 +0200

    Ex4 result extracted

--- ex4/bigrams.py
+++ ex4/bigrams.py
@@ -73,6 +73,8 @@ def pmi(x, y, xy):
     return math.log(xy / (x * y))
 
 
+result_file = open('result.txt', 'w', encoding='utf-8')
+
 unigrams_sum = sum(unigrams.values())
 bigrams_sum = sum(bigrams.values())
 pointwise_mutual_information = {
@@ -80,7 +82,10 @@ pointwise_mutual_information = {
     for k, v in bigrams.items()}
 pmi_list = list(sorted(pointwise_mutual_information.items(), key=lambda kv: kv[1], reverse=True))
 print('Pointwise mutual information')
+result_file.write('Pointwise mutual information\n')
 pprint(pmi_list[:30])
+pprint(str(pmi_list[:30]), stream=result_file)
+result_file.write('\n')
 
 if args.save_to_file:
     with open("scores.txt", 'w', encoding='utf-8') as file:
@@ -94,6 +99,10 @@ llr_diff = llr.llr_compare(Counter(bigrams), Counter(unigrams))
 llr_diff_list = list(sorted(llr_diff.items(), key=lambda kv: kv[1], reverse=True))
 print('Log likelihood ratio')
 pprint(llr_diff_list[:30])
+result_file.write('Log likelihood ratio\n')
+pprint(llr_diff_list[:30])
+pprint(str(llr_diff_list[:30]), stream=result_file)
+result_file.write('\n')
 
 if args.save_to_file:
     with open("scores.txt", 'a', encoding='utf-8') as file:
@@ -101,9 +110,15 @@ if args.save_to_file:
 
 print('Which measure works better for the problem?')
 print('\tLLR > PMI')
+result_file.write('Which measure works better for the problem?\n')
+result_file.write('\tLLR > PMI\n')
 
 print('What would be needed, besides good measure, to build a dictionary of multiword expressions?')
 print('\tLarge amount (or simply enough) of data')
+result_file.write('What would be needed, besides good measure, to build a dictionary of multiword expressions?\n')
+result_file.write('\tLarge amount (or simply enough) of data\n')
 
 print('Can you identify a certain threshold which clearly divides the good expressions from the bad?')
 print('\tNot really, it all depends on data provided')
+result_file.write('Can you identify a certain threshold which clearly divides the good expressions from the bad?\n')
+result_file.write('\tNot really, it all depends on data provided\n')
