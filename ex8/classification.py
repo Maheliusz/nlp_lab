0e5d64f62f0b3f16dcca049a9f8778a7eb383ef2
Michał Zakrzewski <miczak96@gmail.com>
2019-05-12 22:31:00 +0200

    Ex8

--- ex8/classification.py
+++ ex8/classification.py
@@ -1,11 +1,17 @@
 import argparse
 import os
 import random
+from pprint import pprint
 
-import fasttext
+import smart_open
+from fasttext import fasttext
+from flair.data import Sentence
+from flair.data_fetcher import NLPTaskDataFetcher
+from flair.embeddings import WordEmbeddings, FlairEmbeddings, DocumentLSTMEmbeddings
+from flair.models import TextClassifier
+from flair.trainers import ModelTrainer
 from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.metrics import classification_report
-from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import precision_recall_fscore_support
 from sklearn.multiclass import OneVsRestClassifier
 from sklearn.pipeline import Pipeline
 from sklearn.svm import LinearSVC
@@ -83,6 +89,19 @@ if __name__ == '__main__':
             file.write('{} {}\n'.format('__label__normal' if counter < len(normal_training_dir) else '__label__changed',
                                         file_contents.replace('\n', ' ')))
             counter += 1
+    with open('test.txt', 'w', encoding='utf-8') as file:
+        counter = 0
+        for file_contents in testing_data:
+            file.write('{} {}\n'.format('__label__normal' if counter < len(normal_testing_dir) else '__label__changed',
+                                        file_contents.replace('\n', ' ')))
+            counter += 1
+    with open('dev.txt', 'w', encoding='utf-8') as file:
+        counter = 0
+        for file_contents in validation_data:
+            file.write(
+                '{} {}\n'.format('__label__normal' if counter < len(normal_validation_dir) else '__label__changed',
+                                 file_contents.replace('\n', ' ')))
+            counter += 1
 
     grid_search_tune = GridSearchCV(
         pipeline, parameters, cv=2, n_jobs=2, verbose=3)
@@ -119,5 +138,27 @@ if __name__ == '__main__':
     precision = true_positive / (true_positive + false_positive)
     recall = true_positive / (true_positive + false_negative)
     f1 = 2 * (precision * recall) / (precision + recall)
+    precision, recall, f1, _ = precision_recall_fscore_support(list(map(lambda x: x[1], labels)),
+                                                               list(map(lambda x: x[0][0], labels)),
+                                                               labels=['normal', 'changed'])
     print('{}\t\t{}\t\t{}'.format('Precision', 'Recall', 'F1'))
     print('{0:.5f}\t\t{0:.5f}\t\t{0:.5f}'.format(precision, recall, f1))
+
+    corpus = NLPTaskDataFetcher.load_classification_corpus('.', train_file='model.txt', test_file='test.txt',
+                                                           dev_file='dev.txt')
+    word_embeddings = [WordEmbeddings('pl'), FlairEmbeddings('multi-forward'), FlairEmbeddings('multi-backward')]
+    document_embeddings = DocumentLSTMEmbeddings(word_embeddings, hidden_size=512, reproject_words=True,
+                                                 reproject_words_dimension=256)
+    classifier = TextClassifier(document_embeddings, label_dictionary=corpus.make_label_dictionary(), multi_label=False)
+    trainer = ModelTrainer(classifier, corpus)
+    trainer.train('./', max_epochs=1)
+    validation_data = [Sentence(x) for x in validation_data]
+    for x in validation_data:
+        classifier.predict(x)
+    predicted = [x.labels[0].value for x in validation_data]
+    precision, recall, f1, _ = precision_recall_fscore_support(
+        ['normal'] * len(normal_testing_dir) + ['changed'] * len(zmiana_testing_dir),
+        predicted,
+        labels=['normal', 'changed'])
+    print('{}\t\t{}\t\t{}'.format('Precision', 'Recall', 'F1'))
+    print('{}\t\t{}\t\t{}'.format(precision, recall, f1))
