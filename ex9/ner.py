7be1660c813568211397173e13888d1a4807e637
Michał Zakrzewski <miczak96@gmail.com>
2019-05-20 10:50:32 +0200

    Ex9 update

--- ex9/ner.py
+++ ex9/ner.py
@@ -1,7 +1,7 @@
 import argparse
 import os
 import xml.etree.ElementTree as ET
-from textwrap import wrap
+from collections import Counter
 
 import matplotlib.pyplot as plt
 
@@ -43,7 +43,7 @@ for filename in already_parsed:
                 coarse[general_tag].extend(list(subdict.values()))
 
     print('{} processed'.format(filename))
-fig, axs = plt.subplots(2, 1, figsize=(10,30))
+fig, axs = plt.subplots(2, 1, figsize=(10, 30))
 axs[0].bar(list(fine.keys()), list(map(lambda x: len(x), fine.values())))
 axs[1].bar(list(coarse.keys()), list(map(lambda x: len(x), coarse.values())))
 for tag in fine.keys():
@@ -52,3 +52,15 @@ for tag in coarse.keys():
     print('{}\t\t{}'.format(tag, len(coarse[tag])))
 axs[0].tick_params(labelrotation=90)
 fig.savefig('histograms.png')
+fine_counter = []
+coarse_counter = []
+for tag in fine.keys():
+    fine_counter.extend([(y, x, tag) for (x, y) in Counter(fine[tag]).items()])
+fine_counter = sorted(fine_counter, key=lambda elem: elem[0], reverse=True)
+for tag in coarse.keys():
+    coarse_counter.extend([(y, x, tag) for (x, y) in Counter(coarse[tag]).items()])
+coarse_counter = sorted(coarse_counter, key=lambda elem: elem[0], reverse=True)
+with open('top50.txt', 'w', encoding='utf-8') as file:
+    file.write('\n'.join(map(lambda x: str(x), fine_counter[:50])))
+with open('top10.txt', 'w', encoding='utf-8') as file:
+    file.write('\n'.join(map(lambda x: str(x), coarse_counter[:10])))
